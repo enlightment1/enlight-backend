ff92aa8d9a485d0a42b4f6d12a683d83d0f201c9
enlightment1 <zwanelwazi20000@gmail.com>
2026-06-20 22:35:11 +0200

    Delete pushnotifications.js

--- pushnotifications.js
+++
@@ -1,7 +0,0 @@
-const webpush = require('web-push');
-webpush.setVapidDetails('mailto:your@email.com', process.env.VAPID_PUBLIC_KEY, process.env.VAPID_PRIVATE_KEY);
-module.exports = {
-  sendPush: async (subscription, payload) => {
-    await webpush.sendNotification(subscription, JSON.stringify(payload));
-  }
-};
